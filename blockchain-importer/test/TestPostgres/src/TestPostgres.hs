0ccdcc5770abed63d88b31bca89e447c8c3be7cc
Javier Diaz <javier.diaz@iohk.io>
2018-04-26 12:05:43 -0300

    First version of query to Tx table

--- blockchain-importer/test/TestPostgres/src/TestPostgres.hs
+++ blockchain-importer/test/TestPostgres/src/TestPostgres.hs
@@ -7,37 +7,37 @@
 
 module TestPostgres where
 
-import           Opaleye (Column, Nullable, PGBool, PGDate, PGFloat8, PGInt4, PGInt8, PGText, Query,
-                          QueryArr, Table, Unpackspec, aggregate, avg, count, groupBy, ifThenElse,
-                          isNull, leftJoin, matchNullable, pgString, queryTable, restrict, runQuery,
-                          showSqlForPostgres, sum, table, tableColumn, (.&&), (.++), (.<), (.<=),
-                          (.==), (.===))
-
 import           Control.Arrow (returnA)
+import           Data.Maybe (fromMaybe)
 import           Data.Profunctor.Product (p2, p3)
-import           Data.Profunctor.Product.Default (Default)
+import           Data.Profunctor.Product.Default
 import           Data.Profunctor.Product.TH (makeAdaptorAndInstance)
 import qualified Database.PostgreSQL.Simple as PGS
+import           Opaleye
 
+data TxRowPoly a b = TxRow {trId :: a, trAmount :: b} deriving (Show)
 
-testTable :: Table (Column PGInt4, Column PGText)
-                   (Column PGInt4, Column PGText)
-testTable = table "test_table" (p2 (tableColumn "id", tableColumn "str"))
+type TxRow = TxRowPoly String Int
+type TxRowPGW = TxRowPoly (Column PGText) (Column PGInt4)
+type TxRowPGR = TxRowPoly (Column PGText) (Column PGInt4)
 
+$(makeAdaptorAndInstance "pTx" ''TxRowPoly)
 
-testTableQuery :: Query (Column PGInt4, Column PGText)
-testTableQuery = queryTable testTable
+txTable :: Table TxRowPGW TxRowPGR
+txTable = Table "tx" (pTx TxRow {trId = required "id", trAmount = required "amount"})
 
-runTestTableQuery :: PGS.Connection
-                  -> Query (Column PGInt4, Column PGText)
-                  -> IO [(Int, String)]
-runTestTableQuery = runQuery
+getTxs :: IO [TxRow]
+getTxs = do
+  conn <- PGS.connectPostgreSQL "host='localhost' port=5432 dbname='cardano' user='postgres' password='postgres'"
+  runQuery conn query
+  where
+    query :: Opaleye.Query TxRowPGR
+    query = queryTable txTable
 
 test :: IO ()
 test = do
-  conn <- PGS.connectPostgreSQL "host='localhost' port=5432 dbname='cardano' user='postgres' password='postgres'"
-  res <- runTestTableQuery conn testTableQuery
-  print res
+  txs <- getTxs
+  print txs
 
 printSql :: Default Unpackspec a a => Query a -> IO ()
-printSql = putStrLn . maybe "Empty query" id . showSqlForPostgres
+printSql = putStrLn . fromMaybe "Empty query" . showSqlForPostgres
