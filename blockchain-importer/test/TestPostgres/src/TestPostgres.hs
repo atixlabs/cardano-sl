8a8b5a3afc85951ea638a269986615ab7eaa0872
Javier Diaz <javier.diaz@iohk.io>
2018-04-26 12:50:55 -0300

    First version of insert to Tx table

--- blockchain-importer/test/TestPostgres/src/TestPostgres.hs
+++ blockchain-importer/test/TestPostgres/src/TestPostgres.hs
@@ -13,11 +13,15 @@ import           Data.Profunctor.Product (p2, p3)
 import           Data.Profunctor.Product.Default
 import           Data.Profunctor.Product.TH (makeAdaptorAndInstance)
 import qualified Database.PostgreSQL.Simple as PGS
+import           GHC.Int (Int64)
 import           Opaleye
+
+type TxId = String
+type TxAmount = Int
 
 data TxRowPoly a b = TxRow {trId :: a, trAmount :: b} deriving (Show)
 
-type TxRow = TxRowPoly String Int
+type TxRow = TxRowPoly TxId TxAmount
 type TxRowPGW = TxRowPoly (Column PGText) (Column PGInt4)
 type TxRowPGR = TxRowPoly (Column PGText) (Column PGInt4)
 
@@ -26,18 +30,22 @@ $(makeAdaptorAndInstance "pTx" ''TxRowPoly)
 txTable :: Table TxRowPGW TxRowPGR
 txTable = Table "tx" (pTx TxRow {trId = required "id", trAmount = required "amount"})
 
-getTxs :: IO [TxRow]
-getTxs = do
-  conn <- PGS.connectPostgreSQL "host='localhost' port=5432 dbname='cardano' user='postgres' password='postgres'"
-  runQuery conn query
-  where
-    query :: Opaleye.Query TxRowPGR
-    query = queryTable txTable
+getTxs :: PGS.Connection -> IO ()
+getTxs conn = do
+  txs <- runQuery conn (queryTable txTable) :: IO [TxRow]
+  print txs
+
+insertTx :: PGS.Connection -> TxId -> TxAmount -> IO ()
+insertTx conn id amount = do
+  rows <- runInsertMany conn txTable [TxRow (pgString id) (constant amount)]
+  putStrLn $ show rows ++ " row(s) inserted"
 
 test :: IO ()
 test = do
-  txs <- getTxs
-  print txs
+  conn <- PGS.connect PGS.defaultConnectInfo {PGS.connectDatabase = "cardano", PGS.connectPassword = "postgres"}
+  getTxs conn
+  insertTx conn "tx3" 789
+  getTxs conn
 
 printSql :: Default Unpackspec a a => Query a -> IO ()
 printSql = putStrLn . fromMaybe "Empty query" . showSqlForPostgres
