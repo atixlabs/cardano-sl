518d4efcbbe7a9e7ba438f4a9f62b87125ad67ff
Nicolas Tallar <nicolas.tallar@iohk.io>
2018-07-03 09:48:01 -0300

    [PI-230] Removed unused code and comments

---
+++ postgres-consistency/scripts/launch/setup-mainnetDB.sh
@@ -0,0 +1,5 @@
+export DB_USER="postgres"
+export DB_HOST="icarus-poc-backend-db-mainnet.cmngxgxrz3ee.us-east-1.rds.amazonaws.com"
+export DB="icaruspocbackendservice_mainnet"
+export DB_PASSWORD="NASq8R7rRG538GU"
+export DB_PORT=5432
---
+++ postgres-consistency/scripts/launch/setup-stagingDB.sh
@@ -0,0 +1,5 @@
+export DB_USER="postgres"
+export DB_HOST="icarus-poc-backend-db.cmngxgxrz3ee.us-east-1.rds.amazonaws.com"
+export DB="icaruspocbackendservice_prod"
+export DB_PASSWORD="8Cdc3myrToEQIJ"
+export DB_PORT=5432
